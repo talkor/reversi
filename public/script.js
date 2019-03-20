567596869fdb51719eb5c3ac4513ae135b9bd0c2
Tal Koren <talkor@users.noreply.github.com>
2019-03-20 18:37:30 +0200

    Add swapping algorithm, make turns, change colors between turns

--- public/script.js
+++ public/script.js
@@ -1,58 +1,73 @@
 const wrapper = document.querySelector('.wrapper');
 const playerField = document.querySelector('.player-field')
-const m = 8;
-const board = [];
-const initialPos = [27, 28, 36, 35];
+const m = 10;
+const initialPos = [{ x: 4, y:4 }, { x: 5, y: 4 }, { x: 5, y: 5 }, {x: 4, y: 5 }];
 let player1 = true;
 
 init();
 
 function init() {
   renderBoard();
-  setPlayerField();
-  setSquareClickListener();
+  initPlayerField();
   setInitialPos();
-  setGameClock();
+  setSquareClickListener();
+  initGameClock();
+  checkLegalMove(player1);
 }
 
 function renderBoard() {
-  for (let i = 0; i < m * m; i++) {
-    wrapper.innerHTML += `<div class='square' data-position="${i}"><span></span></div>`;
-    
-    board.push({
-      id: i,
-      isBlack: false,
-      isSet: false
-    });
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < m; j++) {
+      wrapper.innerHTML += `<div class='square' data-x="${i}" data-y="${j}"><span></span></div>`;
+    }
   }
 }
 
 function setSquareClickListener() {
+  const colors = ['black', 'white'];
+
   document.querySelectorAll('.square').forEach(square => {
     square.addEventListener('click', event => {
-      square.classList.remove('black');
-      square.classList.remove('white');
-      square.classList.add(player1 ? 'black' : 'white');
-      player1 = !player1;
-      
-      playerField.innerHTML = player1 ? 'Player 1' : 'Player 2';
+      colors.forEach(color => {
+        if (square.classList.contains(`legal-${color}`)) {
+          square.classList.remove(`legal-${color}`);
+          square.classList.add(`${color}`);
+          swapSquares(player1, square.dataset.x, square.dataset.y);
+          prepateNextTurn();
+        }
+      });
     });
   });
 }
 
-function setPlayerField() {
-  playerField.innerHTML = 'Player 1';
+function prepateNextTurn() {
+  player1 = !player1;
+  playerField.innerHTML = player1 ? 'Player 1 (Black)' : 'Player 2 (White)';
+
+  clearLegalMoves();
+  checkLegalMove(player1);
+}
+
+function clearLegalMoves() {
+  document.querySelectorAll('.legal-white, .legal-black').forEach(square => {
+    if (square.classList.contains('legal-white')) square.classList.remove('legal-white');
+    if (square.classList.contains('legal-black')) square.classList.remove('legal-black');
+  });
+}
+
+function initPlayerField() {
+  playerField.innerHTML = `Player 1 (Black)`;
 }
 
 function setInitialPos() {
   let addedClass;
-  initialPos.forEach(squarePos => {
+  initialPos.forEach(pos => {
     addedClass = addedClass === 'white' ? 'black' : 'white';
-    document.querySelector(`.square[data-position="${squarePos}"]`).classList.add(addedClass);
+    document.querySelector(`.square[data-x="${pos.x}"][data-y="${pos.y}"]`).classList.add(addedClass);
   });
 }
 
-function setGameClock() {
+function initGameClock() {
   let minutes = document.querySelector('.minutes');
   let seconds = document.querySelector('.seconds');
   let totalSeconds = 0;
@@ -63,4 +78,38 @@ function setGameClock() {
     seconds.innerHTML = (totalSeconds % 60).toString().padStart(2, '0');;
   }, 1000);
 }
-// TODO: add .lagel class to elements that are legal moves
+
+function swapSquares(player, x, y) {
+  const swappedColor = player ? 'white' : 'black';
+  const newColor = player ? 'black' : 'white';
+
+  const newSquares = [];
+
+  const row = document.querySelectorAll(`.square.${swappedColor}[data-x="${x}"]`);
+  const col = document.querySelectorAll(`.square.${swappedColor}[data-y="${y}"]`);
+  
+  if (row !== undefined && row.length !== 0) newSquares.push(row);
+  if (col !== undefined && col.length !== 0) newSquares.push(col);
+    // newSquares.push(document.querySelectorAll(`.square.black[data-x="${x}"][data-y="${y}"]`));
+
+  for (let i = 0; i < newSquares.length; i++) {
+    newSquares[i][0].classList.remove(swappedColor);
+    newSquares[i][0].classList.add(newColor);
+  }
+}
+
+function checkLegalMove(player){
+  const currentColor = player ? 'black' : 'white';
+  const oppositeColor = player ? 'white' : 'black';
+
+  document.querySelectorAll('.black, .white').forEach(square => {
+    let element;
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          element = document.querySelector(`.square:not(.black):not(.white)[data-x="${parseInt(square.dataset.x) + i}"][data-y="${parseInt(square.dataset.y) + j}"]`);
+          if (element != null)
+            element.classList.add(`legal-${currentColor}`);
+        }
+      }
+  })
+}
